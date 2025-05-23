2dec6419eff1ca0de3619d15c04284691208d6b1
santiago padierna <soypadierna@gmail.com>
2025-05-22 19:22:39 -0500

    Ajuste de diseño segun los cambios de fuente y eliminacion de barra de navegacion

--- pixelart/Form1.Designer.cs
+++ pixelart/Form1.Designer.cs
@@ -36,18 +36,13 @@
             this.btn1 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
             this.btnMake = new System.Windows.Forms.Button();
-            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.misProyectosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.terminadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.pnlBar.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnl1
             // 
-            this.pnl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pnl1.Location = new System.Drawing.Point(90, 170);
+            this.pnl1.BackColor = System.Drawing.Color.Gainsboro;
+            this.pnl1.Location = new System.Drawing.Point(90, 175);
             this.pnl1.Margin = new System.Windows.Forms.Padding(0);
             this.pnl1.Name = "pnl1";
             this.pnl1.Size = new System.Drawing.Size(300, 400);
@@ -55,8 +50,8 @@
             // 
             // pnl2
             // 
-            this.pnl2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pnl2.Location = new System.Drawing.Point(480, 170);
+            this.pnl2.BackColor = System.Drawing.Color.Gainsboro;
+            this.pnl2.Location = new System.Drawing.Point(480, 175);
             this.pnl2.Margin = new System.Windows.Forms.Padding(0);
             this.pnl2.Name = "pnl2";
             this.pnl2.Size = new System.Drawing.Size(300, 400);
@@ -64,8 +59,8 @@
             // 
             // pnl3
             // 
-            this.pnl3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pnl3.Location = new System.Drawing.Point(870, 170);
+            this.pnl3.BackColor = System.Drawing.Color.Gainsboro;
+            this.pnl3.Location = new System.Drawing.Point(870, 175);
             this.pnl3.Margin = new System.Windows.Forms.Padding(0);
             this.pnl3.Name = "pnl3";
             this.pnl3.Size = new System.Drawing.Size(300, 400);
@@ -73,10 +68,9 @@
             // 
             // pnlBar
             // 
-            this.pnlBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pnlBar.Controls.Add(this.menuStrip1);
+            this.pnlBar.BackColor = System.Drawing.Color.Gainsboro;
             this.pnlBar.Controls.Add(this.pnlNav);
-            this.pnlBar.Location = new System.Drawing.Point(230, 40);
+            this.pnlBar.Location = new System.Drawing.Point(230, 48);
             this.pnlBar.Margin = new System.Windows.Forms.Padding(0);
             this.pnlBar.Name = "pnlBar";
             this.pnlBar.Size = new System.Drawing.Size(800, 60);
@@ -84,7 +78,7 @@
             // 
             // pnlNav
             // 
-            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.pnlNav.BackColor = System.Drawing.Color.Gray;
             this.pnlNav.Location = new System.Drawing.Point(200, 15);
             this.pnlNav.Margin = new System.Windows.Forms.Padding(0);
             this.pnlNav.Name = "pnlNav";
@@ -93,7 +87,8 @@
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(90, 590);
+            this.btn1.Location = new System.Drawing.Point(90, 591);
+            this.btn1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(300, 40);
             this.btn1.TabIndex = 4;
@@ -102,7 +97,8 @@
             // 
             // btn2
             // 
-            this.btn2.Location = new System.Drawing.Point(480, 590);
+            this.btn2.Location = new System.Drawing.Point(480, 591);
+            this.btn2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(300, 40);
             this.btn2.TabIndex = 5;
@@ -111,57 +107,17 @@
             // 
             // btnMake
             // 
-            this.btnMake.Location = new System.Drawing.Point(870, 590);
+            this.btnMake.Location = new System.Drawing.Point(870, 591);
+            this.btnMake.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnMake.Name = "btnMake";
             this.btnMake.Size = new System.Drawing.Size(300, 40);
             this.btnMake.TabIndex = 6;
             this.btnMake.Text = "button3";
             this.btnMake.UseVisualStyleBackColor = true;
             // 
-            // inicioToolStripMenuItem
-            // 
-            this.inicioToolStripMenuItem.Margin = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(70, 30);
-            this.inicioToolStripMenuItem.Text = "Inicio";
-            // 
-            // misProyectosToolStripMenuItem
-            // 
-            this.misProyectosToolStripMenuItem.Margin = new System.Windows.Forms.Padding(16, 0, 0, 0);
-            this.misProyectosToolStripMenuItem.Name = "misProyectosToolStripMenuItem";
-            this.misProyectosToolStripMenuItem.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.misProyectosToolStripMenuItem.Size = new System.Drawing.Size(139, 30);
-            this.misProyectosToolStripMenuItem.Text = "Mis Proyectos";
-            // 
-            // terminadosToolStripMenuItem
-            // 
-            this.terminadosToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.terminadosToolStripMenuItem.Name = "terminadosToolStripMenuItem";
-            this.terminadosToolStripMenuItem.Size = new System.Drawing.Size(119, 30);
-            this.terminadosToolStripMenuItem.Text = "Terminados";
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.AutoSize = false;
-            this.menuStrip1.BackColor = System.Drawing.Color.Gainsboro;
-            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(0);
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.inicioToolStripMenuItem,
-            this.misProyectosToolStripMenuItem,
-            this.terminadosToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(200, 15);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(0);
-            this.menuStrip1.Size = new System.Drawing.Size(400, 30);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1258, 744);
             this.Controls.Add(this.btnMake);
@@ -171,13 +127,12 @@
             this.Controls.Add(this.pnl2);
             this.Controls.Add(this.pnl1);
             this.Controls.Add(this.pnlBar);
-            this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Font = new System.Drawing.Font("Rubik", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.pnlBar.ResumeLayout(false);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -191,10 +146,6 @@
         private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Button btn2;
         private System.Windows.Forms.Button btnMake;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem misProyectosToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem terminadosToolStripMenuItem;
     }
 }
 
--- pixelart/Form1.cs
+++ pixelart/Form1.cs
@@ -31,5 +31,10 @@ namespace pixelart
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
